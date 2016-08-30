e8d3072d718f63416894007fa60f5e2488642efe
joes <hi@wem.me>
2016-08-30 11:51:04 +0800

    Add files

--- services/parsers/wrappers/index.js
+++ services/parsers/wrappers/index.js
@@ -1,1 +1,9 @@
+// Express
+exports.express = require('./express');
+
+// DB
+exports.redis = require('./redis');
+exports.ioredis = require('./ioredis');
+exports.memcached = require('./memcached');
 exports.mongodb = require('./mongodb');
+exports.mysql = require('./mysql');
