b4168d47e3024962402a6e83ac5a4d3d7086cdda
joes <hi@wem.me>
2016-09-06 18:08:39 +0800

    delete unused

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 var tingyun = require('tingyun');
 var express = require('express');
-var bodyParser = require('body-parser');
 var Q = require('q');
 var wrappers = require('./services/parsers/wrappers');
 require('./dbs/mongodb');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-app.use(bodyParser.json());
-
-app.use('/', routes);
-app.use('/users', users);
 
 function toObj(action_data) {
     var obj = {};
